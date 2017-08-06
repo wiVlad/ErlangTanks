82f12f00afa8ab432598c6ca97dd27697d5d7e55
jonoliel <oliel.jonathan@gmail.com>
2017-08-06 20:41:50 +0300

    Alpha 1.4

--- src/include/ErlangTanks.hrl
+++
@@ -1,9 +0,0 @@
-%% parque header file
-
--define(max_x,(1080)).
--define(max_y,(720)).
--define(SHELL_INTERVAL, 20).
--define(CRATE_RANGE_INTERVAL, 7000).
--define(CRATE_MIN_INTERVAL, 7000).
--define(CRATE_INTERVAL, 100).
--define(inRange(Xself,Xother,Yself,Yother), ((abs((Xother) - (Xself+5)) < 30) and (abs((Yother)-(Yself+5))<30))).
